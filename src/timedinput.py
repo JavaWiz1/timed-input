90e7ad4cc58f72edd9105decafc0f49f970b0e28
Al DAmico <JavaWiz1@hotmail.com>
2021-10-24 20:03:52 -0400

    Restructure - Handle backspace

--- src/timedinput.py
+++ src/timedinput.py
@@ -13,10 +13,24 @@ def _input_with_timeout_win(prompt, timeout_secs: int, timer=time.monotonic) -> 
     result = []
     while timer() < endtime:
         if msvcrt.kbhit():
-            result.append(msvcrt.getwche()) #XXX can it block on multibyte characters?
-            if result[-1] == '\r':   #XXX check what Windows returns here
+            endtime = timer() + timeout_secs  # Reset the timer
+            ch = msvcrt.getwche()
+            # result.append(msvcrt.getwche())   # XXX can it block on multibyte characters?
+            if ch == "\b":
+                if len(result) >= 1:
+                    result = result[:-1]
+                    print(' \b', end="", flush=True) 
+                else:
+                    # ESC [ <n> C  (move n chars right)
+                    move_right = "\x1B\x5B\x31\x43"
+                    print(move_right, end='', flush=True)
+            elif ch == '\r':            # XXX check what Windows returns here
                 print()
-                return ''.join(result[:-1])
+                # return ''.join(result[:-1])
+                return ''.join(result)
+            else:
+                result.append(ch)
+                
         time.sleep(0.04) # just to yield to other processes/threads
     print()
     raise TimeoutError('Time Expired.')
@@ -48,7 +62,7 @@ def input_with_timeout(prompt: str, timeout_secs: int, default: str = None) -> s
         return default
 
 if __name__ == "__main__":
-    timeout = 10
+    timeout = 60
     default_response = "This is the default response!"
     response = input_with_timeout("The Prompt> ", timeout, default_response)
     print(f'Input prompt returns: {response}')
